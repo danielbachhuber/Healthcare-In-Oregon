6368f8604c719505c8f3a054bc8a08402a2470d2
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-02 14:49:55 -0800

    Add Easy Mode Auto Deploy

--- content/mu-plugins/0-loader.php
+++ content/mu-plugins/0-loader.php
@@ -1,7 +1,8 @@
 <?php
 
 $load_mu_plugins = array(
-	'co-authors-plus/co-authors-plus.php'
+	'co-authors-plus/co-authors-plus.php',
+	'easy-mode-auto-deploy/easy-mode-auto-deploy.php',
 	);
 
 foreach( $load_mu_plugins as $mu_plugin ) {
